183a0d1945cf6f543f28b0d83d7302419e46155a
Pavan Kumar <pavanrnri1818@gmail.com>
2023-01-17 21:47:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var a = 1;
+function showerhide1(){
+    if(a==1){
+        document.getElementById('greyboxout_1').style.display = "flex";
+        document.getElementById('plus_btn_1').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_1').style.display = "none";
+        document.getElementById('plus_btn_1').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
+function showerhide2(){
+    if(a==1){
+        document.getElementById('greyboxout_2').style.display = "flex";
+        document.getElementById('plus_btn_2').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_2').style.display = "none";
+        document.getElementById('plus_btn_2').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
+function showerhide3(){
+    if(a==1){
+        document.getElementById('greyboxout_3').style.display = "flex";
+        document.getElementById('plus_btn_3').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_3').style.display = "none";
+        document.getElementById('plus_btn_3').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
+function showerhide4(){
+    if(a==1){
+        document.getElementById('greyboxout_4').style.display = "flex";
+        document.getElementById('plus_btn_4').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_4').style.display = "none";
+        document.getElementById('plus_btn_4').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
+function showerhide5(){
+    if(a==1){
+        document.getElementById('greyboxout_5').style.display = "flex";
+        document.getElementById('plus_btn_5').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_5').style.display = "none";
+        document.getElementById('plus_btn_5').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
+function showerhide6(){
+    if(a==1){
+        document.getElementById('greyboxout_6').style.display = "flex";
+        document.getElementById('plus_btn_6').style.transform = 'rotate(45deg)';
+        return a=0;
+    }
+    else{
+        document.getElementById('greyboxout_6').style.display = "none";
+        document.getElementById('plus_btn_6').style.transform = 'rotate(90deg)';
+        return a=1;
+    }
+}
